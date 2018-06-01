a00c7793ecd95d216dbd453f07b74fd39ee066ab
Anas Mohamed <amohamed@ualberta.ca>
2018-06-01 09:30:41 -0600

    Updated Subreddit Settings

--- Redditbot.py
+++ Redditbot.py
@@ -4,13 +4,12 @@ import configparser
 import praw
 
 reddit = praw.Reddit('bot1')
-subreddit = reddit.subreddit('')
+subreddit = reddit.subreddit('all')
 
-for submission in subreddit.new(limit=2):
+for submission in subreddit.rising(limit=5):
     url = submission.url
-    score = submission.score
 
-    if 'facebook' in url and 'video' in url and score > 20:
+    if 'facebook' in url and 'video' in url:
         config = configparser.ConfigParser()
         config.read("config.ini")
         email = config.get('streamable_info', 'email')
@@ -21,7 +20,7 @@ for submission in subreddit.new(limit=2):
             link = 'https://streamable.com/' + str(response['shortcode'])
             cat_link = 'http://thecatapi.com/api/images/get?format=src&type=gif'
             
-            submission.reply('''Streamable Mirror: [Link](%s)  \n*******  \n Hi, I am a bot. I take Facebook video links and return Streamable Mirrors, why? cause Facebook sucks  \nFeel free to pm me about any errors, my master check occasionally. I think ಠ_ಠ  \nPsst, here is a [random cat gif](%s), why? cause cats don't suck unlike Faceboo. ''' % (link, cat_link))
+            submission.reply('''Streamable Mirror: [Link](%s)  \n*******  \n Hi, I am a bot. I take Facebook video links and return Streamable Mirrors, why? cause Facebook sucks  \nFeel free to pm me about any errors, my master check occasionally.  \nPsst, here is a [random cat gif](%s), why? cause cats don't suck unlike Facebook. ''' % (link, cat_link))
         except requests.exceptions.RequestException:
             pass
     else:
